b3b85d2c1de277e0e4657dbd35311c2dda34f329
Karel E. Nakladal <karel.e.nakladal@gmail.com>
2026-01-12 01:39:55 +0100

    database

--- database.sql
+++ database.sql
@@ -5,6 +5,19 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP TABLE IF EXISTS `access_tokens`;
+CREATE TABLE `access_tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_access_id` int(11) NOT NULL,
+  `user_tokens_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_access_id` (`user_access_id`),
+  KEY `user_tokens_id` (`user_tokens_id`),
+  CONSTRAINT `access_tokens_ibfk_1` FOREIGN KEY (`user_access_id`) REFERENCES `user_accesses` (`id`),
+  CONSTRAINT `access_tokens_ibfk_2` FOREIGN KEY (`user_tokens_id`) REFERENCES `user_tokens` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
+
 DROP TABLE IF EXISTS `addresses`;
 CREATE TABLE `addresses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -154,6 +167,7 @@ CREATE TABLE `product_variants` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) NOT NULL,
   `price` int(11) NOT NULL,
+  `sku` varchar(50) DEFAULT NULL,
   `stock` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
   `edited_at` datetime DEFAULT NULL,
@@ -181,20 +195,55 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
+  `avatar` varchar(100) NOT NULL DEFAULT 'img/persistent/default/user.png',
   `password` varchar(100) NOT NULL,
-  `firstname` varchar(50) NOT NULL,
-  `lastname` varchar(50) NOT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
   `email` varchar(50) NOT NULL,
   `phone` varchar(15) DEFAULT NULL,
+  `two_factor` enum('TRUE','FALSE') NOT NULL DEFAULT 'FALSE',
   `roles` enum('CUSTOMER','VIP_CUSTOMER','CURATOR','EDITOR','MODERATOR','ACCOUNTANT','INVENTORY','MANAGER','ADMIN') NOT NULL DEFAULT 'CUSTOMER',
+  `verified_at` datetime DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `edited_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 
-INSERT INTO `users` (`id`, `username`, `password`, `firstname`, `lastname`, `email`, `phone`, `roles`, `created_at`, `edited_at`, `deleted_at`) VALUES
-(1,	'admin',	'$2y$12$y5pbzLlIkV/t1FqYnsHNE..tTkF398DLZhLttinUOM7.fjQ64pn1S',	'Admin',	'Admin',	'admin@admin.admin',	NULL,	'ADMIN',	'2026-01-10 19:44:44',	NULL,	NULL);
+INSERT INTO `users` (`id`, `username`, `avatar`, `password`, `firstname`, `lastname`, `email`, `phone`, `two_factor`, `roles`, `verified_at`, `created_at`, `edited_at`, `deleted_at`) VALUES
+(1,	'admin',	'img/persistent/default/user.png',	'$2y$12$y5pbzLlIkV/t1FqYnsHNE..tTkF398DLZhLttinUOM7.fjQ64pn1S',	'Admin',	'Admin',	'admin@admin.admin',	NULL,	'FALSE',	'ADMIN',	NULL,	'2026-01-10 19:44:44',	NULL,	NULL),
+(14,	'karel',	'img/volatile/avatars/karel.jpeg',	'$2y$12$9vLF69.ErIws20PFv6ewwODmvH1TxDSzx47CBqnxcDwhc0m2FAU7u',	NULL,	NULL,	'nakladalkarel@gmail.com',	NULL,	'FALSE',	'CUSTOMER',	'2026-01-11 00:14:09',	'2026-01-11 00:13:10',	'2026-01-11 23:03:39',	NULL);
 
--- 2026-01-10 20:36:09
+DROP TABLE IF EXISTS `user_accesses`;
+CREATE TABLE `user_accesses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `users_id` int(11) NOT NULL,
+  `from` varchar(50) NOT NULL,
+  `remember` enum('TRUE','FALSE') NOT NULL DEFAULT 'FALSE',
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `users_id` (`users_id`),
+  CONSTRAINT `user_accesses_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
+
+DROP TABLE IF EXISTS `user_tokens`;
+CREATE TABLE `user_tokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `token_hash` varchar(64) NOT NULL,
+  `type` enum('VERIFICATION','PASSWORDRESET','ADDRESSBLOCK','CANCELATION','TWOFACTOR','API') NOT NULL,
+  `created_at` datetime NOT NULL,
+  `expires_at` datetime NOT NULL,
+  `used_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `user_tokens_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
+INSERT INTO `user_tokens` (`id`, `user_id`, `token_hash`, `type`, `created_at`, `expires_at`, `used_at`, `deleted_at`) VALUES
+(21,	14,	'5af17404c13b39728dee929accbb4467e4de4540bfddfef882008446ec8c5f94',	'VERIFICATION',	'2026-01-11 00:13:10',	'2026-01-12 00:13:10',	'2026-01-11 00:14:09',	NULL),
+(22,	14,	'3553282d5b69f837f732470bdd2e1ec7f88a1ae6ede22027be7677f3d02217b7',	'CANCELATION',	'2026-01-11 00:13:10',	'2026-01-12 00:13:10',	NULL,	'2026-01-11 00:14:09');
+
+-- 2026-01-12 00:39:19
